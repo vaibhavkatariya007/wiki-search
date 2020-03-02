13e01b6bf5c1ad0f2ef5bb4abbafd9e9e33c3cde
ACCIONLABS\AL2342 <vaibhav.katariya@accionlabs.com>
2020-03-02 14:21:09 +0530

    Updates

---
+++ precache-manifest.e35123d6ddd425bd154eaed6f3894b42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a6e59b3bff983863be232a1933228f",
+    "url": "/wiki-search/index.html"
+  },
+  {
+    "revision": "b0f5b6c2738efd608937",
+    "url": "/wiki-search/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "771cb5af000c1859822e",
+    "url": "/wiki-search/static/css/main.aa052139.chunk.css"
+  },
+  {
+    "revision": "b0f5b6c2738efd608937",
+    "url": "/wiki-search/static/js/2.df1f83a4.chunk.js"
+  },
+  {
+    "revision": "6194aa18dbe32bb6cf4c",
+    "url": "/wiki-search/static/js/3.0e634867.chunk.js"
+  },
+  {
+    "revision": "74c7fcb6620e9b55b8a7",
+    "url": "/wiki-search/static/js/4.af35ba3b.chunk.js"
+  },
+  {
+    "revision": "9fba83daa7cbd21fc203",
+    "url": "/wiki-search/static/js/5.c1647a78.chunk.js"
+  },
+  {
+    "revision": "771cb5af000c1859822e",
+    "url": "/wiki-search/static/js/main.52419b4a.chunk.js"
+  },
+  {
+    "revision": "df98152d6bd3940ee373",
+    "url": "/wiki-search/static/js/runtime~main.4baf23d0.js"
+  }
+]);
